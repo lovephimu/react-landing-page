a5c93b2dbb0e16693c1143d9d2e29ba16887c7d3
lovephimu <soe.nlb@gmail.com>
2023-05-09 22:29:34 +0200

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,27 +1,46 @@
 import './App.css';
 import React from 'react';
 import styles from './App.module.scss';
-import { ReactComponent as logoOne } from './logodysnix.svg';
-import { ReactComponent as logoTwo } from './logorpc.svg';
+import { ReactComponent as LogoTwo } from './logodysnix.svg';
+import { ReactComponent as LogoOne } from './logorpc.svg';
 
 export default function App() {
   return (
     <>
       <header>
-        <div>
-          <logoOne />
-          <logoTwo />
-          <span>Web3 API</span>
-          <span>Enterprise</span>
-          <span>Pricing</span>
-          <span>Docs</span>
-          <span>Blog</span>
-          <span>About Us</span>
-          <span>Log in</span>
-          <span>Try it for free</span>
+        <div className={styles.flexHeaderMenu}>
+          <LogoOne className={styles.flexImage} />
+          <LogoTwo className={styles.flexImage} />
+          <a href="/#" className={styles.flexHeaderItems}>
+            Web3 API
+          </a>
+          <a href="/#" className={styles.flexHeaderItems}>
+            Enterprise
+          </a>
+          <a href="/#" className={styles.flexHeaderItems}>
+            Pricing
+          </a>
+          <a href="/#" className={styles.flexHeaderItems}>
+            Docs
+          </a>
+          <a href="/#" className={styles.flexHeaderItems}>
+            Blog
+          </a>
+          <a href="/#" className={styles.flexHeaderItems}>
+            About Us
+          </a>
+          <a href="/#" className={`${styles.flexHeaderItems} ${styles.logIn}`}>
+            Log in
+          </a>
+          <a
+            href="/#"
+            className={`${styles.flexHeaderItems} ${styles.linkButton}`}
+          >
+            Try it for free
+          </a>
         </div>
       </header>
-      <main></main>
+      <section></section>
       <footer></footer>
     </>
   );
