a7c69494bb22a2cee43d4c697a08797759ebd681
lovephimu <soe.nlb@gmail.com>
2023-05-09 20:34:38 +0200

    Add header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import './App.css';
+import React from 'react';
 import styles from './App.module.scss';
+import { ReactComponent as logoOne } from './logodysnix.svg';
+import { ReactComponent as logoTwo } from './logorpc.svg';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <header>
+        <div>
+          <logoOne />
+          <logoTwo />
+          <span>Web3 API</span>
+          <span>Enterprise</span>
+          <span>Pricing</span>
+          <span>Docs</span>
+          <span>Blog</span>
+          <span>About Us</span>
+          <span>Log in</span>
+          <span>Try it for free</span>
+        </div>
+      </header>
+      <main></main>
+      <footer></footer>
+    </>
+  );
 }
